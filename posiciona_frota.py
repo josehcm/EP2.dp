00a9a9b9c49f78f6c1a1ccd9992f067d0b292602
Rafael Agnelo <rafaelca13@al.insper.edu.br>
2023-01-27 10:40:37 -0300

    posiciona frota

---
+++ posiciona_frota.py
@@ -0,0 +1,23 @@
+def posiciona_frota(frota): 
+
+    tabuleiro = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+    for posicao in frota.values():
+        for lista in posicao:
+            for lista_azul in lista:
+                x = lista_azul[0]
+                y = lista_azul[1]
+                tabuleiro[x][y] = 1
+                
+    return tabuleiro     
